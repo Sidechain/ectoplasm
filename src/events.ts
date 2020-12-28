c038dc42cea38fafb712cbf57922d3619d0d05e1
Alexander Erni <alexander.c.erni@gmail.com>
2020-12-28 20:10:08 +0100

    Add events generator and database

---
+++ src/events.ts
@@ -0,0 +1,8 @@
+import awokengod from './images/awokengod.jpg'
+
+export default [{
+    title: "Darkness falls",
+    text: "Dark clouds sweep over the skies, and what little light it you before is now all gone. You squint in the dark, hoping to be able to see what lurks in the shadows, but you know that you're inevitably trapped in this world and the hope inside you is the only thing lighting your path from now on.",
+    image: awokengod,
+    modifier: {stat:"sight", effect:-5}
+}]
